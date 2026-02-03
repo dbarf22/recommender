cf0381cc23c22570786c4642e59bf03528bba7b0
Dylan Barfield <dbarfield4@gmail.com>
2026-02-03 13:53:30 -0500

    M

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,24 @@
+import type { PageServerLoad } from './$types';
+import { supabase } from '$lib/supabaseClient';
+
+type MediaType = 'movie' | 'show' | 'book' | 'album';
+
+type Recommendation = {
+	id: number;
+	title: string;
+	description: string;
+	image_link: string;
+	type: MediaType;
+};
+
+export const load: PageServerLoad = async () => {
+	const { data, error } = await supabase.from('posts').select<'posts', Recommendation>();
+	console.log("Done with await")
+	if (error) {
+		console.error('Error loading instruments:', error.message);
+		return { posts: [] };
+	}
+	return {
+		posts: data ?? []
+	};
+};
