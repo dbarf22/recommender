679638730cfb17eb22dfe8dd61bca492e80f6570
Dylan Barfield <dbarfield4@gmail.com>
2026-02-02 23:33:58 -0500

    Basic UI down

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,51 @@
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
+	let postModal: HTMLDialogElement | undefined = $state();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+
+
+<nav class="navbar bg-base-100 shadow-sm">
+	<a class="btn btn-ghost text-xl" href="/">hi</a>
+</nav>
+
+<div class="fab">
+	<button class="btn btn-xl btn-circle btn-primary" onclick={() => {postModal?.showModal()}}>+</button>
+</div>
+
+<dialog bind:this={postModal} class="modal">
+	<div class="modal-box">
+		<div class="flex flex-col gap-4">
+			<h3 class="text-lg font-bold">Enter a recommendation</h3>
+			<select class="select w-full">
+				<option disabled selected>What are you recommending?</option>
+				<option>A movie</option>
+				<option>An album</option>
+				<option>A TV show</option>
+				<option>A book</option>
+			</select>
+			<input type="text" placeholder="Title" class="input w-full">
+			
+			<textarea class="textarea textarea-bordered w-full" placeholder="Write a recommendation"></textarea>
+		
+		</div>
+		
+		
+		<div class="modal-action">
+			<form method="dialog">
+				<!-- if there is a button in form, it will close the modal -->
+				<button class="btn btn-neutral">Cancel</button>
+			</form>
+			
+			<button class="btn btn-primary">Submit</button>
+		</div>
+	</div>
+	
+	
+</dialog>
+
+
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+</script>
+
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 2xl:grid-cols-3 gap-4 p-4 ">
+	
+	<div class="card card-side bg-base-100 shadow-sm h-64">
+		<figure class="h-full w-auto shrink-0 p-4 ">
+			<img
+					src="https://www.themoviedb.org/t/p/w600_and_h900_face/63N9uy8nd9j7Eog2axPQ8lbr3Wj.jpg"
+					alt="Movie Poster"
+					class="h-full w-full object-cover rounded-md"
+			/>
+		</figure>
+		<div class="card-body overflow-hidden">
+				<h2 class="card-title">Blade Runner</h2>
+				<p class="line-clamp-6 overflow-hidden">Test test test teeeest</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Listen</button>
+			</div>
+		</div>
+	</div>
+	
+	<div class="card card-side bg-base-100 shadow-sm h-64 overflow-hidden">
+		<figure class="h-full w-auto shrink-0 p-4">
+			<img
+					src="https://upload.wikimedia.org/wikipedia/en/5/51/Kendrick_Lamar_-_Damn.png"
+					alt="Movie Poster"
+					class="h-full w-full object-cover rounded-md"
+			/>
+		</figure>
+		<div class="card-body overflow-hidden ">
+
+				<h2 class="card-title">DAMN</h2>
+				<p class="wrap-normal line-clamp-4">afddfasdfsd faf fad </p>
+
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Listen</button>
+			</div>
+		</div>
+	</div>
+	
+
+</div>
